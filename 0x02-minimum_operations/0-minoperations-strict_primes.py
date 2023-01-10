911a34d3fbcdbb41fb1e73b9ac9d7cef1fc4f902
AlX-kim <100015164+AlX-kim@users.noreply.github.com>
2023-01-10 12:10:26 +0300

    Add files via upload

--- 0x02-minimum_operations/0-minoperations-strict_primes.py
+++ 0x02-minimum_operations/0-minoperations-strict_primes.py
@@ -19,32 +19,27 @@ def minOperations(n):
         the minimum number of operations needed to result in n characters
         or 0 if n is impossible to achieve
     """
-    # minOperations will be the sum of all prime factors of n
-    # Check if given a valid positive int greater than 1
     if type(n) is not int or n <= 1:
         return 0
-    # Start summation list to store divisors
+    primes = [2, 3]
     summation = []
-    # start divisors at 2, first prime number
     divisor = 2
-    # divide until no longer divisible or dividing by itself
     while (n % divisor) is 0 and (n // divisor) is not 1:
-        # each time dividing, add divisor to list to sum later
         summation.append(divisor)
-        # update the number to show the division
         n = n // divisor
-    # update divisor to 3, next prime number
     divisor = 3
-    # keep going until number is less than divisor
     while n > divisor:
-        # keep completing divisions with new odd numbers
         while (n % divisor) is 0 and (n // divisor) is not 1:
             summation.append(divisor)
             n = n // divisor
-        # move divisor to next odd number
-        # not-strictly prime, but later odd numbers are comprised of previous
         divisor += 2
-    # add the number itself, last prime number
+        while True:
+            for prime in primes:
+                if (divisor % prime) is 0:
+                    divisor += 2
+                    break
+            else:
+                primes.append(divisor)
+                break
     summation.append(n)
-    # return the sum of all prime factors
     return sum(summation)
